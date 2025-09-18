34049f7ec892379d20bf471dc7410ca8af7d472b
takumi-tokunaga <takumi_tokunaga@hakko.co.jp>
2025-09-18 23:22:35 +0900

    Starting Minecraft Backuper...

--- minecraft_backuper/minecraft_backuper.py
+++ minecraft_backuper/minecraft_backuper.py
@@ -53,9 +53,9 @@ class LogHandler(FileSystemEventHandler):
                                 "--keep-monthly=3"
                                 ], check=True)
                             
-                        break
-                    else:
-                        print("No player join/leave events detected in recent log lines.")
+                            break
+                        else:
+                            print("No player join/leave events detected in recent log lines.")
             except Exception as e:
                 print(f"Error processing log file: {e}")
 
