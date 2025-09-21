eaf51bc0775d76bb6b58cbd411cd3c3b272eab3f
takumi-tokunaga <takumi_tokunaga@hakko.co.jp>
2025-09-21 11:32:34 +0900

    パス修正

--- minecraft_backuper/minecraft_backuper.py
+++ minecraft_backuper/minecraft_backuper.py
@@ -66,7 +66,7 @@ class LogHandler(FileSystemEventHandler):
 print(f"Monitoring log file: {LOG_PATH}")
 
 observer = Observer()
-observer.schedule(LogHandler(LOG_PATH), path=LOG_PATH, recursive=False)
+observer.schedule(LogHandler(LOG_PATH), path="/data/logs", recursive=False)
 observer.start()
 
 try:
